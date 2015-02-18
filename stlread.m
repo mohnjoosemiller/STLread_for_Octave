2f1fc67f1acaa593bd500f7d54c7fa1674d79b44
John Moosemiller <itcaughtonj@gmail.com>
2015-02-17 23:41:38 -0600

    Clarity and if()

--- stlread.m
+++ stlread.m
@@ -1,10 +1,6 @@
 function [vertices, faces] = stlread(filename)
-% This function reads an STL file in binary format into matrixes X, Y and
-% Z, and C.  C is optional and contains color rgb data in 5 bits.  
-%
-% USAGE: [x, y, z, c] = stlread(filename);
-%
-% To plot use patch(x,y,z,c), or patch(x,y,z)
+% This function reads an STL file in binary format into matrixes vertices
+% and faces which can be used by patch to view the stl.
 %
 % MATLAB code by Doron Harlev
 % Octave edits by John Moosemiller
@@ -38,7 +34,7 @@ for i=1:num_facet
     ver2=fread(fid,3,'float32'); % vertex 2
     ver3=fread(fid,3,'float32'); % vertex 3
     col=fread(fid,1,'uint16'); % color bytes
-    if (bitget(col,16)==1 & use_color)
+    if bitget(col,16)==1 & use_color
         r=bitshift(bitand(2^16-1, col),-10);
         g=bitshift(bitand(2^11-1, col),-5);
         b=bitand(2^6-1, col);
@@ -49,10 +45,7 @@ for i=1:num_facet
     vertices(3*i , :) = [ver3(1),ver3(2),ver3(3)];
     
     faces(i, :) = [3*i-2,3*i-1,3*i];
-    
-   % x(:,i)=[ver1(1); ver2(1); ver3(1)]; % convert to matlab "patch" compatible format
-   % y(:,i)=[ver1(2); ver2(2); ver3(2)];
-   % z(:,i)=[ver1(3); ver2(3); ver3(3)];
+
 end
 if use_color
     varargout(1)={c};
